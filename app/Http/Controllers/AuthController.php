833c210fc4681e94a7bf54834c85aaaae8f47adb
carmentenorio <carmen.tenorio.btsoft@gmail.com>
2025-09-17 17:50:35 +0200

    implement login, register and logout (#7)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required|string|max:255',
+            'email'=> 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()
+            ->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            return response()
+                ->json(['message' => 'Unathorized'], 401);
+        }
+        $user  = User::where('email', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()
+            ->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer'], 201);
+    }
+
+    public function me()
+    {
+        return Auth::user();
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()
+            ->json([
+            'message'=> 'You have sucessfully logged out and the token was sucessfully deleted',
+            ], 201);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,14 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, Notifiable, HasFactory;
     /**
      * The attributes that are mass assignable.
      *
@@ -35,7 +37,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'=> 'hashed',
         ];
     }
 }
